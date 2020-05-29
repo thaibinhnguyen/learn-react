e38dcf3a5085ad63c120771cec9ca3b7cf816861
THAI Binh Nguyen <thaibinhnguyen26041996@gmail.com>
2020-05-29 17:15:31 +0200

    update like component

--- src/components/common/Like.jsx
+++ src/components/common/Like.jsx
@@ -2,17 +2,16 @@ import React, { Component } from "react";
 
 // Input: liked: boolean
 // Output: onClick
-export default class Like extends Component {
-    render() {
-        let classes = "fa fa-heart";
-        if (!this.props.liked) classes += "-o";
-        return (
-            <i
-                onClick={this.props.onClick}
-                style={{ cursor: "pointer" }}
-                className={classes}
-                aria-hidden="true"
-            ></i>
-        );
-    }
+
+export default function Like({ liked, onClick }) {
+    let classes = "fa fa-heart";
+    if (!liked) classes += "-o";
+    return (
+        <i
+            onClick={onClick}
+            style={{ cursor: "pointer" }}
+            className={classes}
+            aria-hidden="true"
+        ></i>
+    );
 }
