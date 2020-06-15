67855cd90add9afd8e7772ff22bc9ff2bc86c1ff
THAI Binh Nguyen <thaibinhnguyen26041996@gmail.com>
2020-06-15 17:36:52 +0200

    add react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import Movies from "./components/Movies";
+import Navbar from "./components/common/Navbar";
 
 export default class App extends Component {
     render() {
         return (
             <React.Fragment>
+                <Navbar />
                 <main className="main">
                     <Movies />
                 </main>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>,
+    document.getElementById("root")
+);
