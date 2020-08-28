3b866debb9bf6ca55083c3f9f23be616ee0e79df
THAI Binh Nguyen <thaibinhnguyen26041996@gmail.com>
2020-08-28 18:12:17 +0200

    handling multiple inputs

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,15 +1,26 @@
 import React, { Component } from "react";
 
 export default class LoginForm extends Component {
-    username = React.createRef();
-
+    // username = React.createRef();
+    state = {
+        account: {
+            password: "",
+            username: ""
+        }
+    }
     handleSubmit = (e) => {
         e.preventDefault();
         //call the server
         const username = this.username.current.value;
         console.log("submitted");
     };
+    handleChange = ({ currentTarget: input }) => {
+        const account = { ...this.state.account };
+        account[input.name] = input.value;
+        this.setState({ account })
+    }
     render() {
+        const { account } = this.state;
         return (
             <div>
                 <h1>Login</h1>
@@ -18,7 +29,9 @@ export default class LoginForm extends Component {
                         <label htmlFor="username">Username</label>
                         <input
                             autoFocus
-                            ref={this.username}
+                            name="username"
+                            value={account.username}
+                            onChange={this.handleChange}
                             id="username"
                             type="text"
                             className="form-control"
@@ -27,9 +40,12 @@ export default class LoginForm extends Component {
                     <div className="form-group">
                         <label htmlFor="password">Password</label>
                         <input
+                            name="password"
                             id="password"
                             type="text"
                             className="form-control"
+                            value={account.password}
+                            onChange={this.handleChange}
                         />
                     </div>
                     <button className="btn btn-primary">Login</button>
